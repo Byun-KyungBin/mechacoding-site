1d3b82a8b113eecfd5328936767d68caa4ac58f4
Byun-KyungBin <87975596+Byun-KyungBin@users.noreply.github.com>
2026-06-04 01:18:15 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'Noto Sans KR', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
