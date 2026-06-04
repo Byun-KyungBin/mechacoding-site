d5161674b992bf960912760c131a0298c772feaa
Byun-KyungBin <87975596+Byun-KyungBin@users.noreply.github.com>
2026-06-04 12:48:19 +0900

    Create sitemap.ts

---
+++ sitemap.ts
@@ -0,0 +1,32 @@
+// app/sitemap.ts
+
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.mechacoding.kr",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.mechacoding.kr/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.mechacoding.kr/programs",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.mechacoding.kr/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
