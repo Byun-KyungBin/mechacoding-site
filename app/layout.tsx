b463e2ce85f60683f7d0de240924ba3707c2ba30
Byun-KyungBin <87975596+Byun-KyungBin@users.noreply.github.com>
2026-06-12 00:04:13 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,8 +71,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko" className="scroll-smooth">
       <head>
-        <link rel="icon" type="image/png" href="/images/favicon.ico" />
-        <link rel="apple-touch-icon" href="/images/favicon.ico" />
+        <link rel="icon" type="image/png" href="public/images/favicon.ico" />
+        <link rel="apple-touch-icon" href="public/images/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=Noto+Sans+KR:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
       </head>
